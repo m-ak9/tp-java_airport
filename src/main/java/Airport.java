ddf74362f58382a2481ef5ca01e9b002d5afe0fa
Mickael <akninemickael@gmail.com>
2021-07-23 12:43:17 +0200

    exo 2

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -1,7 +1,10 @@
+import java.util.List;
+
 public class Airport {
     String name;
     Position position;
     LandingStripe landingStripe;
+    List<Airplane> proximityAirplane;
 
     public Airport(String name, Position position, LandingStripe landingStripe) {
         this.name = name;
@@ -9,5 +12,26 @@ public class Airport {
         this.landingStripe = landingStripe;
     }
 
+    private void detectProximityAirplane(List<Airplane> allAirplane) {
+        allAirplane.forEach( plane -> {
+            if (plane.currentPosition.)
+        });
 
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public void setLandingStripe(LandingStripe landingStripe) {
+        this.landingStripe = landingStripe;
+    }
+
+    public void setProximityAirplane(List<Airplane> proximityAirplane) {
+        this.proximityAirplane = proximityAirplane;
+    }
 }
